555935971e30cbddfb87ff2ef86b93585d26daf5
MarvelSQ <sun1993hlj@hotmail.com>
2021-07-11 21:52:22 +0800

    fix(build): format sub_path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const { SUB_PATH } = process.env;
+const { REPOSITORY_PATH, OWNER } = process.env;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: `/${SUB_PATH}/`,
+  base: `/${REPOSITORY_PATH.replace(`${OWNER}/`,'')}/`,
 });
